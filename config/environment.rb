cc8368abb886efa86a811c8d1c957f2e04ab3061
MathieuParadis <mathieuyl.paradis@gmail.com>
2021-05-17 06:49:51 +0100

    test2

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,7 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+config.action_mailer.delivery_method = :smtp
 
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_LOGIN'],
