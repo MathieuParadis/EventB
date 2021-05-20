bf636939e9098668cedc87679b05b28f2e0e5fa4
MathieuParadis <mathieuyl.paradis@gmail.com>
2021-05-20 11:25:20 +0100

    commit jeudi

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,6 +1,6 @@
 class AttendancesController < ApplicationController
   def index
-    @event_id = params[:format]
+    @event_id = params[:event_id]
     @event = Event.find(@event_id)
   
     @attendances = Attendance.where(event: @event)
@@ -8,24 +8,52 @@ class AttendancesController < ApplicationController
   end
 
   def new
-    @event_id = params[:format]
+    @event_id = params[:event_id]
     @event = Event.find(@event_id) 
   end
 
   def create
-    @event_id = params[:format]
+    # Before the rescue, at the beginning of the method
+    @stripe_amount = 500
+
+    @event_id = params[:event_id]
     @event = Event.find(@event_id) 
     user = current_user
 
-    @attendance = Attendance.new(event: @event, guest: user) 
+    begin
+      
+      customer = Stripe::Customer.create({
+      email: params[:stripeEmail],
+      source: params[:stripeToken],
+      })
+
+      charge = Stripe::Charge.create({
+      customer: customer.id,
+      amount: @stripe_amount,
+      description: "Achat d'un produit",
+      currency: 'eur',
+      })
+
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to @attendance
+    end
+
+    @attendance = Attendance.new(event: @event, guest: user, stripe_customer_id: customer.id) 
+
 
     if @attendance.save 
       flash[:Notice] = "You enrolled succesfully"
-      redirect_to @attendance
+      redirect_to @event
     else
       flash.now[:notice] = @attendance.errors.full_messages
       render :new
     end
+
+
   end
+
+
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root to: 'events#index'
-  # get 'contact', to: 'static_pages#contact'
-  # get 'team', to: 'static_pages#team'
-  # get 'welcome/:user_entry', to: 'welcome#show'
-  # resources :sessions, only: [:new, :create, :destroy]
   resources :users  
-  resources :events
-  resources :attendances
-
+  resources :events do
+    resources :attendances
+  end
 
 end
